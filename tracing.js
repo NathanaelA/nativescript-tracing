9b6a04362b0bdeaf298177faf201091a1a0d6cb7
Nathanael Anderson <nathan@master-technology.com>
2015-06-02 20:17:26 -0500

    Add the Levels code

--- tracing.js
+++ tracing.js
@@ -2,14 +2,20 @@
  * (c) 2015, Master Technology
  * Licensed under the MIT license or contact me for a Commercial or Support License
  *
+ * I do contract work in most languages, so let me solve your problems!
+ *
  * Any questions please feel free to email me or put a issue up on the github repo.
- * Version 0.0.1                                       Nathan@master-technology.com
+ * Version 0.0.2                                       Nathan@master-technology.com
  **********************************************************************************/
 "use strict";
 
+/* jshint camelcase: false */
+/* global global */
+
 if (!console || !console.log) {
     var Console = require('console/console-native').Console;
-    console = new Console();
+    //noinspection JSClosureCompilerSyntax
+    global.console = new Console();
 }
 
 var tracer = function(traceme, options) {
@@ -25,8 +31,11 @@ function traceWrapper(func, name) {
     return function() {
         if (this._MT_tracing === false) {
             return func.apply(this, arguments);
-        }  else {
-            var args = [];
+        } else {
+            var Inc = [];
+            for (var j=0;j<this._MT_tracing_level;j++) { Inc.push("   "); }
+            Inc = Inc.join();
+            var args = [Inc];
             args.push("Starting " + name + (arguments.length ? ":" : ""));
             for (var i = 0; i < arguments.length; i++) {
                 if (typeof arguments[i] === "function") {
@@ -36,11 +45,13 @@ function traceWrapper(func, name) {
                 }
             }
             console.log.apply(console, args);
+            this._MT_tracing_level++;
             var res = func.apply(this, arguments);
+            this._MT_tracing_level--;
 			if (typeof arguments[0] === 'function') {
-				console.log("Ending ", name, "(callback)");
+				console.log(Inc, "Ending ", name, "(callback)");
 			} else {
-			   console.log("Ending ", name); //, arguments[0] ? arguments[0] : '');
+			   console.log(Inc, "Ending ", name);
             }
 			return res;
         }
@@ -51,15 +62,18 @@ function traceWrapper(func, name) {
 var addTracing = function(traceme, options) {
     var ignore = options.ignore || [];
     for (var name in traceme) {
-        if (ignore[name]) continue;
         if (traceme.hasOwnProperty(name) && typeof traceme[name] === 'function') {
+            if (ignore[name]) { continue; }
             traceme[name] = traceWrapper(traceme[name], name);
         }
     }
-
+    traceme._MT_tracing_level = 0;
     if (!options.disableAddedFunction) {
         traceme.tracing = function(e) {
-            this._MT_tracing = !!e;
+            if (arguments.length) {
+                this._MT_tracing = !!e;
+            }
+            return !!this._MT_tracing;
         };
     }
 };
